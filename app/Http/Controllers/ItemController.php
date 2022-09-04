5983ef55178de76b31bbb99ce9fd579cafed71e0
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-09-04 16:44:04 +0900

    38. ItemSeeder、ナビメニュー

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Items/Index', ['items' => Item::all()]);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UsersTableSeeder::class,
+            ItemsTableSeeder::class,
         ]);
     }
 }
