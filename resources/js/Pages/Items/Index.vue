533d252b4049f60fb56fa62d1ebd6c3da5941083
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-09-04 17:48:37 +0900

    39. Items(商品・サービス)index

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -1,6 +1,10 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+
+defineProps({
+  items: Array,
+})
 </script>
 
   <template>
@@ -18,7 +22,39 @@ import { Head } from '@inertiajs/inertia-vue3';
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
-            You're logged in!
+            <section class="text-gray-600 body-font">
+              <div class="container px-5 py-8 mx-auto">
+                <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
+                  <button
+                    class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+                </div>
+                <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                  <table class="table-auto w-full text-left whitespace-no-wrap">
+                    <thead>
+                      <tr>
+                        <th
+                          class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                          ID</th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                          商品名</th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                          価格</th>
+                        <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                          スタータス</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="item in items" :key="item.id">
+                        <td class="px-4 py-3">{{ item.id }}</td>
+                        <td class="px-4 py-3">{{ item.name }}</td>
+                        <td class="px-4 py-3">{{ item.price }}</td>
+                        <td class="px-4 py-3 text-lg">{{ item.is_selling }}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </div>
