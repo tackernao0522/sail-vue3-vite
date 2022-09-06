78a1b5fb43ca0ebfba4ce7b727cfce0d2a0f9a9c
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-09-06 21:10:12 +0900

    62. Purchase モデル・マイグレーション・リレーション

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\Customer::factory(1000)->create();
+        \App\Models\Purchase::factory(100)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
