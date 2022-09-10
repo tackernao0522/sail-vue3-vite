f12f10d9b72b3c4752c591da4c59a7c6f4bc1105
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-09-10 15:02:15 +0900

    94-Api通信(Ajax通信)の通信確認

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -2,6 +2,7 @@
 import { getToday } from '@/common';
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import axios from 'axios';
 import { onMounted, reactive } from 'vue';
 
 
@@ -12,8 +13,27 @@ onMounted(() => {
 
 const form = reactive({
     startDate: null,
-    endDate: null
+    endDate: null,
+    type: 'perDay' // 仮で直入力
 })
+
+const getData = async () => {
+    try {
+        await axios.get('/api/analysis/', {
+            params: {
+                startDate: form.startDate,
+                endDate: form.endDate,
+                type: form.type
+            }
+        })
+            .then((res) => {
+                // data.value = res.data
+                console.log(res.data)
+            })
+    } catch (e) {
+        console.log(e.message)
+    }
+}
 </script>
 
 <template>
@@ -30,18 +50,11 @@ const form = reactive({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <form @submit.prevent="">
-                            From: <input
-                                type="date"
-                                name="startDate"
-                                v-model="form.startDate"
-                            >
-                            To: <input
-                                type="date"
-                                name="endDate"
-                                v-model="form.endDate"
-                            ><br>
-                            <button class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
+                        <form @submit.prevent="getData">
+                            From: <input type="date" name="startDate" v-model="form.startDate">
+                            To: <input type="date" name="endDate" v-model="form.endDate"><br>
+                            <button
+                                class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
                         </form>
                     </div>
                 </div>
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AnalysisController;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,7 @@ Route::middleware('auth:sanctum')->get('/searchCustomers', function (Request $re
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware('auth:sanctum')->get('/analysis', [AnalysisController::class, 'index'])
+    ->name('api.analysis');
