f3abb0d331f7d5e494ae6b836c6fb79299e5f553
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-09-11 17:48:41 +0900

    107. API通信でデシル分析結果を取得

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -119,7 +119,7 @@ class AnalysisController extends Controller
         round(100 * totalPerGroup / @total, 1) as totalRatio
         ')->get();
 
-        dd($data);
+        // dd($data);
 
         return Inertia::render('Analysis');
     }
